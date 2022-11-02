12d601bf6ee192a742b08c7bf14a9eafa666ce39
norre-daroy <norrej.daroy@gmail.com>
2022-11-01 23:13:25 -0400

    Add player input function

--- cse-210-01/tictactoe.py
+++ cse-210-01/tictactoe.py
@@ -6,3 +6,24 @@
 board = ["1", "2", "3",
         "4", "5", "6",
         "7", "8", "9"]
+
+# Ask for player input    
+def playerInput(board, currentPlayer):
+        if currentPlayer == 'x':
+            # Ask player x to input chosen spot
+            inputX = int(input("x's turn to choose a square (1-9): "))
+             # Check if chosen spot is already taken by checking if it's neither o and x.
+            if board[inputX-1] != "o" and board[inputX-1] != "x":
+                board[inputX-1] = "x"
+            else:
+                # If spot is already filled, then its next players move
+                print("Spot already filled!!")
+        else:
+            # Ask player o to input chosen spot
+            inputO = int(input("o's turn to choose a square (1-9): "))
+            # Check if chosen spot is already taken by checking if it's neither o and x.
+            if board[inputO-1] != "o" and board[inputO-1] != "x":
+                board[inputO-1] = "o"
+            else:
+                # If spot is already filled, then its next players move
+                print("Spot already filled!!")
