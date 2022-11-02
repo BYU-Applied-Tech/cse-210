8afde38c9e2281ae5a42022436c48e2fde0666aa
norre-daroy <norrej.daroy@gmail.com>
2022-11-01 23:14:09 -0400

    Add display game board function

--- cse-210-01/tictactoe.py
+++ cse-210-01/tictactoe.py
@@ -7,6 +7,14 @@ board = ["1", "2", "3",
         "4", "5", "6",
         "7", "8", "9"]
 
+# Display game board
+def displayGameBoard(board):
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("--+---+--")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("--+---+--")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    
 # Ask for player input    
 def playerInput(board, currentPlayer):
         if currentPlayer == 'x':
